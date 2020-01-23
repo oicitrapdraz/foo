f86748b440621c8449c4014eddbe8151a9fe2e53
Patricio Sard <patriciosard@gmail.com>
2020-01-23 19:01:06 -0300

    test: add Foo::FileParser tests

---
+++ spec/foo/file_parser_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Foo::FileParser do
+  context 'when file exists with correct syntax' do
+    before :all do
+      open('valid_complete.sudoku', 'w') do |f|
+        f.puts '8 5 0 |0 0 2 |4 0 0'
+        f.puts '7 2 0 |0 0 0 |0 0 9'
+        f.puts '0 0 4 |0 0 0 |0 0 0'
+        f.puts '------+------+------'
+        f.puts '0 0 0 |1 0 7 |0 0 2'
+        f.puts '3 0 5 |0 0 0 |9 0 0'
+        f.puts '0 4 0 |0 0 0 |0 0 0'
+        f.puts '------+------+------'
+        f.puts '0 0 0 |0 8 0 |0 7 0'
+        f.puts '0 1 7 |0 0 0 |0 0 0'
+        f.puts '0 0 0 |0 3 6 |0 4 0'
+      end
+    end
+
+    it 'should correctly parse its content' do
+      lines = Foo::FileParser.new('valid_complete.sudoku').parse
+
+      expect(lines).to eq(
+        [
+          [8, 5, 0, 0, 0, 2, 4, 0, 0],
+          [7, 2, 0, 0, 0, 0, 0, 0, 9],
+          [0, 0, 4, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 1, 0, 7, 0, 0, 2],
+          [3, 0, 5, 0, 0, 0, 9, 0, 0],
+          [0, 4, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 8, 0, 0, 7, 0],
+          [0, 1, 7, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 3, 6, 0, 4, 0]
+        ]
+      )
+    end
+
+    after :all do
+      if File.exist?('valid_complete.sudoku')
+        File.delete('valid_complete.sudoku')
+      end
+    end
+  end
+
+  context "when file doesn't exists" do
+    it 'should raise an exception' do
+      expect { Foo::FileParser.new('non_existing_file.sudoku').parse }.to raise_error(Errno::ENOENT)
+    end
+  end
+end
